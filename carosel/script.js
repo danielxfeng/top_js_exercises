e8844205370a979a0dd93d0825d88e3373320ddb
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-09-17 19:06:52 +0300

    Finished Carosel

--- carosel/script.js
+++ carosel/script.js
@@ -2,46 +2,70 @@ let container = document.getElementById("carosel_container");
 
 let baseDiv = document.createElement("div");
 baseDiv.className = "carosel_base";
-
-for (let i = 1; i <= 5; i++) {
+for (let i = 0; i < 5; i++) {
   let img = document.createElement("img");
-  img.src = `./images/${i}.webp`;
+  img.src = `./images/${i + 1}.webp`;
   img.alt = `carosel_img_${i}`;
   img.className = "carosel_img";
   baseDiv.appendChild(img);
 }
 
-let box = document.createElement("div");
-box.className = "carosel_box";
-box.appendChild(baseDiv);
+let btns = document.createElement("div");
+btns.className = "carosel_btns";
+for (let i = 0; i < 2; i++) {
+  let btn = document.createElement("div");
+  btn.className = "carosel_btn";
+  btn.setAttribute("id", `carosel_btn_${i}`);
+  btn.textContent = i === 0 ? "<" : ">";
+  btn.addEventListener("click", () => {btnClick(i)});
+  btns.appendChild(btn);
+}
 
 let dots = document.createElement("div");
 dots.className = "carosel_dots";
-for (let i = 1; i <= 3; i++) {
+for (let i = 0; i < 5; i++) {
   let dot = document.createElement("div");
   dot.className = "carosel_dot";
   dot.setAttribute("id", `carosel_dot_${i}`);
-  dot.textContent = "•";
+  if (i === 0) {
+    dot.textContent = "●";
+  } else {
+    dot.textContent = "○";
+  }
+
+  dot.addEventListener("click", () => {dotClick(i)});
   dots.appendChild(dot);
 }
 
-let bigBox = document.createElement("div");
-bigBox.className = "carosel_big_box";
-bigBox.appendChild(box);
-bigBox.appendChild(dots);
+container.appendChild(baseDiv);
+container.appendChild(btns);
+container.appendChild(dots);
 
-let left = document.createElement("div");
-left.className = "carosel_btn";
-left.setAttribute("id", "carosel_btn_left");
-left.innerHTML = "<";
+let currPic = 0;
 
-let right = document.createElement("div");
-right.className = "carosel_btn";
-right.setAttribute("id", "carosel_btn_right");
-right.innerHTML = ">";
+function dotClick(idx) {
+  let dots = document.getElementsByClassName("carosel_dot");
+  for (let dot of dots) {
+    dot.textContent = "○";
+  }
+  dots[idx].textContent = "●";
+  currPic = idx;
+  let baseDiv = document.getElementsByClassName("carosel_base")[0];
+  baseDiv.style.transform = `translateX(-${idx * 510}px)`;
+}
 
-container.appendChild(left);
-container.appendChild(bigBox);
-container.appendChild(right);
+function btnClick(dir) {
+  let idx = currPic;
+  if (dir === 0) {
+    idx = idx === 0 ? idx = 4 : idx - 1;
+  } else {
+    idx = idx === 4 ? idx = 0 : idx + 1;
+  }
+  dotClick(idx);
+}
 
+function autoChange() {
+  setInterval(()=>{btnClick(1)}, 5000);
+}
 
+autoChange();
